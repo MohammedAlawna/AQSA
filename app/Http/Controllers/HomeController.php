7116edddcbd94e48928d41eb74fb412e30ec81d9
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2022-02-27 23:36:26 +0000

    displayDoctorData

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\Doctor;
 
 
 class HomeController extends Controller
@@ -12,7 +13,8 @@ class HomeController extends Controller
     public function redirect() {
         if(Auth::id()) {
             if(Auth::user()->usertype == '0'){
-                return view('user.home');
+                $doctor = doctor::all();
+                return view('user.home', compact('doctor'));
             }
             else {
                 return view('admin.home');
@@ -26,6 +28,7 @@ class HomeController extends Controller
     }
 
     public function index() {
-        return view('user.home');
+        $doctor = doctor::all();
+        return view('user.home',compact('doctor'));
     }
 }
